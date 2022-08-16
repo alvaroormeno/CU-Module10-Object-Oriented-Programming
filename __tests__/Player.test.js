ea36f695bbe99aef8036b1b1432904f8181a4abd
alvaroormeno <alvaro@ormeno.org>
2022-08-16 14:49:31 -0400

    almost done with playe object

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -41,3 +41,37 @@ test('gets inventory from player or returns false', () => {
 
   expect(player.getInventory()).toEqual(false)
 })
+
+// HEALTH TEST
+test('gets players health value', () => {
+
+  const player = new Player('Dave');
+
+  expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()))
+})
+
+// IS ALIVE? TEST
+test('checks if player is alive or not', () => {
+
+  const player = new Player('Dave');
+
+  expect(player.isAlive()).toBeTruthy()
+
+  player.health = 0
+
+  expect(player.isAlive()).toBeFalsy()
+})
+
+// HEALTH SUBSTRACTION TEST
+test('subtracts from players health', () => {
+  const player = new Player('Dave');
+  const oldHealth = player.health
+
+  player.reduceHealth(5);
+
+  expect(player.health).toBe(oldHealth - 5)
+
+  player.reduceHealth(99999)
+
+  expect(player.health).toBe(0)
+})
